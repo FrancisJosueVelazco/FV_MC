ebff102691d80dba6fa43a03e75a91d6d22fa871
fvelazco <francisvl1998@gmail.com>
2019-07-23 17:44:40 -0500

    Transaccionale exitoso

--- mediapp-backend/src/main/java/com/fvelazco/dao/IConsultaExamenDAO.java
+++ mediapp-backend/src/main/java/com/fvelazco/dao/IConsultaExamenDAO.java
@@ -1,7 +1,10 @@
 package com.fvelazco.dao;
 
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,7 +12,8 @@ import org.springframework.stereotype.Repository;
 import com.fvelazco.model.ConsultaExamen;
 @Repository
 public interface IConsultaExamenDAO extends JpaRepository<ConsultaExamen, Integer>{
-	
+	@Transactional
+	@Modifying
 	@Query(value = "INSERT INTO consulta_examen(id_consulta,id_examen)VALUES(:idConsulta,:idExamen)",nativeQuery = true)
 	Integer registrar(@Param ("idConsulta")Integer idConsulta,@Param("idExamen")Integer idExamen);
  
