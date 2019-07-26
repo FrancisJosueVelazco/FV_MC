803eb854daf81f7eb164988868eb2dc36f1f2b64
fvelazco <francisvl1998@gmail.com>
2019-07-26 09:45:21 -0500

    Atrasado

--- mediapp-backend/src/main/java/com/fvelazco/controller/ConsultaController.java
+++ mediapp-backend/src/main/java/com/fvelazco/controller/ConsultaController.java
@@ -1,9 +1,12 @@
 package com.fvelazco.controller;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,26 +14,35 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fvelazco.model.Consulta;
+import com.fvelazco.model.ConsultaDTO;
 import com.fvelazco.model.ConsultaListaExamenDTO;
 import com.fvelazco.service.ConsultaService;
 
 @RestController
 @RequestMapping("/consultas")
 public class ConsultaController {
-	
+
 	@Autowired
 	private ConsultaService service;
-	
-	
+
 	@PostMapping(value = "/registrar", produces = "application/json", consumes = "application/json ")
 	public ResponseEntity<Object> registrar(@RequestBody ConsultaListaExamenDTO consDTO) {
 		Consulta consulta = new Consulta();
-		consulta= service.registrarTransaccional(consDTO);
-		
+		consulta = service.registrarTransaccional(consDTO);
+
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(consulta.getIdConsulta()).toUri();
-		
+
 		return ResponseEntity.created(location).build();
+	}
+
+	
+	@GetMapping(value = "/hateoas", produces = "application/json")
+	public List<ConsultaDTO> listarHateoas() {
+		List<Consulta> consultas = new ArrayList<>();
+		List<ConsultaDTO> consultaDTO = new ArrayList<>();
+		consultas = service.listar();
+
 	}
 
 }
---
+++ mediapp-backend/src/main/java/com/fvelazco/model/ConsultaDTO.java
@@ -0,0 +1,30 @@
+package com.fvelazco.model;
+
+import org.springframework.hateoas.ResourceSupport;
+
+public class ConsultaDTO extends ResourceSupport{
+
+	private Integer idConsulta;
+	private Medico medico;
+	private Paciente paciente;
+	public Integer getIdConsulta() {
+		return idConsulta;
+	}
+	public void setIdConsulta(Integer idConsulta) {
+		this.idConsulta = idConsulta;
+	}
+	public Medico getMedico() {
+		return medico;
+	}
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+	public Paciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+	
+	
+}
